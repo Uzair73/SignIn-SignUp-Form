39ffb9970f30abed8ce235c7475445af29abeed0
uzair <uzairhakeem71@gmail.com>
2024-04-14 10:06:05 +0500

    change done

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const sign_in_btn = document.querySelector("#sign-in-btn");
 const sign_up_btn = document.querySelector("#sign-up-btn");
 const container = document.querySelector(".container");
-const name = document.getElementById("Name");
-const user_name = document.getElementById("Username");
-const pass = document.getElementById("Password");
-console.log(name,user_name,pass)
+const name = document.getElementById("name");
+const user_name = document.getElementById("email");
+const pass = document.getElementById("password");
 
 // sign_up_btn.addEventListener('click', () =>{
 //     container.classList.add("sign-up-mode");
@@ -18,13 +17,18 @@ console.log(name,user_name,pass)
  const thisForm = document.getElementById('myform');
 thisForm.addEventListener('submit', async function (e) {
     e.preventDefault();
-    const formData = new FormData(thisForm).entries()
+    const formData = {
+        name: name.value,
+        email: user_name.value,
+        password: pass.value
+    };
     const response = await fetch('http://localhost:5000/auth/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(Object.fromEntries(formData))
+        body: JSON.stringify(formData)
     });
 
     const result = await response.json();
     console.log(result)
+    window.location.href = 'https://www.ilmkidunya.com/11th-class/notes/biology-federal-board#google_vignette'
 });
