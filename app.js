4ef598bd883c01668dbddf3ca96af29a2c32a23e
UZAIR HAKEEM <151864936+Uzair73@users.noreply.github.com>
2024-04-15 10:19:14 +0500

    Update app.js

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ thisForm.addEventListener('submit', async function (e) {
         name: name.value,
         password: pass.value
     };
-    const response = await fetch('http://localhost:5000/auth/login', {
+    const response = await fetch('https://login-page-tau-six.vercel.app/auth/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData)
